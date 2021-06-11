3e69d1940060abb4575ae66848fff05a4b0f65c2
Produde's Predetor <bigtimegangstar@gmail.com>
2021-06-11 21:32:28 +0700

    Fix auto watering wrong condition, improve log by creating AutoWateringLog ref on firebase!

--- api/notificationService.js
+++ api/notificationService.js
@@ -18,8 +18,8 @@ async function sendNotification(sensor_data){
     var check_service = new plantSettingService(sensor_data.plant_id);
     await check_service.get_setting_data();
     var [check_result, check_sign] = check_service.check_env_threshold(sensor_data);
-    if (check_result){
-        if (check_service.settings_data.water_mode === true){
+    if (check_result ){
+        if (check_service.settings_data.water_mode === true && check_sign === '<' && sensor_data.name === 'SOIL'){
             autoWater(sensor_data, check_service.settings_data);
             return
         }
@@ -33,8 +33,8 @@ async function sendNotification(sensor_data){
         console.log(
             'Type: ', sensor_data.name, 
             'Val: ', sensor_data.data, 
-            'Thresh_max: ', check_service.get_threshold(sensor_data,'>'),
-            'Thresh_min: ', check_service.get_threshold(sensor_data,'<'))
+            'Thresh_min: ', check_service.get_threshold(sensor_data,'<'),
+            'Thresh_max: ', check_service.get_threshold(sensor_data,'>'))
     }
 }
 function push_to_firebase(firebase_json){
@@ -51,17 +51,35 @@ function autoWater(sensor_data,plant_settings){
         data:'1',
         unit:""
     }
-    console.log('-----------------------------------------------------')
-    console.log('Water on from autoWatering, current moisture: ',sensor_data.data, ' threshold: ', plant_settings.min_moist, 'water_ammoint: ', plant_settings.water_amount)
+    date = new Date().toISOString();
     water_interval = plant_settings.water_amount / 100 * 5 //NOTE: 100ml -> 5 secs
     global.mqttClient2.publish(global.adaInfo.feed_relay, JSON.stringify(json_data));
     setTimeout(() => {
         json_data.data = '0';
-        console.log('Water off from autoWatering!, expected interval: ', water_interval)
         global.mqttClient2.publish(global.adaInfo.feed_relay, JSON.stringify(json_data));
+        push_auto_water_log(date,sensor_data,plant_settings,water_interval,plant_settings.water_amount);
     }, water_interval * 1000);
 
 
+}
+const ref_autowater_log = firebase.database().ref('AutoWaterLog')
+ref_autowater_log.on('child_added',snapshot => {
+    console.log('------------------------')
+    console.log('Auto water data received')
+    console.log(JSON.stringify(snapshot.val()))
+    console.log('----------------------------')
+})
+function push_auto_water_log(date,sensor_data,plant_settings,water_interval,water_amount){
+    var json = {
+        date : date,
+        data : sensor_data.data,
+        min_moist: plant_settings.min_moist,
+        water_amount: water_amount,
+        water_interval: water_interval,
+        type: 'AUTO'
+    }
+    let new_ref = ref_autowater_log.push()
+    new_ref.set(json)
 }
 exports.sendNotification = sendNotification;
 
